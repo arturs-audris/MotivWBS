493cd4f1e63bdea3636ed861ab4183b29f11290c
arturs.audris <arturs.audris@gmail.com>
2017-11-24 16:47:21 +0200

    Added bootstrap, updated page design, started work on date filtering

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,31 +1,76 @@
-function tableCreate(data){
-    var body = document.body,
-        tbl  = document.createElement('table');
-    tbl.style.width  = '100px';
-    tbl.style.border = '1px solid black';
+function tableCreate(data) {
+    var root = document.getElementById("todoTable"),
+        tbl = document.createElement('table');
+    tbl.setAttribute("id", "todoEntries");
+    tbl.setAttribute("class", "table table-filter");
+    var tr = tbl.insertRow();
+    var td = tr.insertCell();
+    td.appendChild(document.createTextNode("ID"));
+    var td = tr.insertCell();
+    td.appendChild(document.createTextNode("Description"));
+    var td = tr.insertCell();
+    td.appendChild(document.createTextNode("Due Date"));
 
-    for(var i = 0; i < data.length; i++){
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1; //January is 0!
+    var yyyy = today.getFullYear();
+
+    if (dd < 10) {
+        dd = '0' + dd
+    }
+
+    if (mm < 10) {
+        mm = '0' + mm
+    }
+    today = mm + '/' + dd + '/' + yyyy;
+
+
+    for (var i = 0; i < data.length; i++) {
         var tr = tbl.insertRow();
-        for(var j = 0; j < 3; j++){
-            if(i == data.length&& j == 1){
+        for (var j = 0; j < 3; j++) {
+            if (i == data.length && j == 1) {
                 break;
             } else {
                 var td = tr.insertCell();
-                if (j == 0) {td.appendChild(document.createTextNode(data[i].id));}
-                if (j == 1) {td.appendChild(document.createTextNode(data[i].description));}
-                if (j == 2) {td.appendChild(document.createTextNode(data[i].dueDate));}
-                td.style.border = '1px solid black';
+                if (j == 0) {
+                    td.appendChild(document.createTextNode(data[i].id));
+                }
+                if (j == 1) {
+                    td.appendChild(document.createTextNode(data[i].description));
+                }
+                if (j == 2) {
+                    td.appendChild(document.createTextNode(data[i].dueDate));
+                    var dueMonth = data[i].dueDate.substr(0, 2);
+                    var dueDay = data[i].dueDate.substr(3, 2);
+                    var dueYear = data[i].dueDate.substr(6, 4);
+
+                    if (dueYear < yyyy || (dueMonth < mm && dueDay < dd && dueYear <= yyyy)) {
+                        if (data[i].completed != true) {
+                            td.setAttribute("class", "overdue");
+                        } else {
+                            td.setAttribute("class", "finished");
+                        }
+                    } else {
+                        if (data[i].completed != true) {
+                            td.setAttribute("class", "pending");
+                        } else {
+                            td.setAttribute("class", "finished");
+                        }
+
+                    }
+
+                }
             }
         }
     }
-    body.appendChild(tbl);
+    root.appendChild(tbl);
 }
 
 $(document).ready(function () {
-        document.getElementById('feature').innerHTML = "Not many features implemented yet"
-    $.getJSON("/todo.json", function(data){
+    $.getJSON("/todo.json", function (data) {
         tableCreate(data)
     });
-    });
+});
 
 
--- src/main/webapp/jsp/index.jsp
+++ src/main/webapp/jsp/index.jsp
@@ -1,15 +1,57 @@
-<%@ page contentType="text/html;charset=UTF-8"%>
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ page contentType="text/html;charset=UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
-  <head>
+
+<head>
     <title>MotivWBS</title>
+    <link href="<c:url value="/css/bootstrap.min.css" />" rel="stylesheet" type="text/css">
     <link href="<c:url value="/css/index.css" />" rel="stylesheet" type="text/css">
-      <script src="<c:url value="/js/jquery-3.2.1.min.js" />"></script>
-      <script src="<c:url value="/js/index.js" />"></script>
-  </head>
-  <body>
-    <div class="heading">TODO List</div>
-    <div id="feature"></div>
-<div id="singleEntry"></div>
-  </body>
+    <script src="<c:url value="/js/jquery-3.2.1.min.js" />"></script>
+    <script src="<c:url value="/js/bootstrap.bundle.min.js" />"></script>
+    <script src="<c:url value="/js/index.js" />"></script>
+</head>
+<body>
+<div id="wrapper">
+    <header>
+        <div class="container-fluid">
+            <h1 id="logo">
+                TODO List
+            </h1>
+        </div>
+    </header>
+    <div id="main">
+        <div id="content">
+            <section>
+                <div class="col-md-8 col-md-offset-2">
+                    <div class="panel panel-default">
+                        <div class="panel-body">
+                            <div class="pull-right">
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-success btn-filter" data-target="done">Done
+                                    </button>
+                                    <button type="button" class="btn btn-danger btn-filter" data-target="overdue">
+                                        Overdue
+                                    </button>
+                                    <button type="button" class="btn btn-warning btn-filter" data-target="pending">
+                                        Pending
+                                    </button>
+                                    <button type="button" class="btn btn-default btn-filter" data-target="all">All
+                                    </button>
+                                </div>
+                            </div>
+                            <div class="table-container">
+                                <div class="table table-filter">
+                                    <div id="todoTable">
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+</div>
+</body>
 </html>
