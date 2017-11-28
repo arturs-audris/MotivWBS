b84cdefb20e016f781dfa50458f7621de661297e
arturs.audris <arturs.audris@gmail.com>
2017-11-28 11:58:37 +0200

    Swapped DB to Postgres, removed test modules

--- src/main/java/my/training/motivwbs/configs/MvcConfiguration.java
+++ src/main/java/my/training/motivwbs/configs/MvcConfiguration.java
@@ -1,4 +1,4 @@
-package my.training.motivwbs;
+package my.training.motivwbs.configs;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
